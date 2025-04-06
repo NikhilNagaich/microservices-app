099f945f5d7a4bfcd5185cfb9b96160832fe0b1b
NikhilNagaich <117948617+NikhilNagaich@users.noreply.github.com>
2025-04-06 18:07:33 +0530

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,41 +1,21 @@
-from flask import Flask
-import psycopg2
-from celery import Celery
-
-celery_app = Celery(
-    'tasks',
-    broker='redis://redis:6379/0',
-    backend='redis://redis:6379/0'
-)
+from flask import Flask, jsonify, request
+import os
+import redis
+import json
 
 app = Flask(__name__)
-
-# Connect to PostgreSQL
-def get_db_connection():
-    conn = psycopg2.connect(
-        host="db",
-        database="mydatabase",
-        user="myuser",
-        password="mypassword"
-    )
-    return conn
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
+r = redis.from_url(redis_url)
 
 @app.route('/')
-def index():
-    name = "Nikhil Nagaich"
-    roll_number = "2022BCD0032"
-    bio = "I am a student at IIIT Kottayam, pursuing my Bachelors in Computer Science Engineering with specialization in Aritificial Intelligence and Data Science."
+def home():
+    return jsonify({'message': 'Web Service is Running'}), 200
 
-    return f"""
-    <h1>Hello, {name} ({roll_number})!</h1>
-    <p>{bio}</p>
-    <p>Visit /task to add a task to the worker queue.</p>
-    """
-
-@app.route('/task')
-def add_task():
-    celery_app.send_task("worker.process_task")  # Send task using Celery
-    return "Task added to the worker queue!"
+@app.route('/task', methods=['POST'])
+def create_task():
+    data = request.get_json()
+    r.rpush('task_queue', json.dumps(data))
+    return jsonify({'message': 'Task added to queue'}), 202
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8081)
+    app.run(host='0.0.0.0', port=5000)
